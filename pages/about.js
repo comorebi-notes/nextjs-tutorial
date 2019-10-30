9d518c47d991722a882c22e68714ffefb8dd861f
kero <kero@comore.bi>
2019-10-30 12:14:31 +0900

    ページ作成

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const About = () => (
+  <div>
+    <p>This is the about page</p>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </div>
+)
+
+export default About

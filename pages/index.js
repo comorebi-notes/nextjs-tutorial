57a49dfe43846405a26e3f06aa8e79d3e49ccb4c
kero <kero@comore.bi>
2019-10-30 13:04:05 +0900

    URL を動的に生成

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
 import Layout from '../components/Layout'
+import Link from 'next/link'
+
+const PostLink = ({ id }) => (
+  <li>
+    <Link href="/posts/[id]" as={`/posts/${id}`}>
+      <a>{id}</a>
+    </Link>
+  </li>
+)
 
 const Index = () => (
   <Layout>
     <p>Hello Next.js</p>
+    <hr />
+    <ul>
+      {[
+        'hello-nextjs',
+        'learn-nextjs',
+        'deploy-nextjs'
+      ].map((id) => <PostLink key={id} id={id} />)}
+    </ul>
   </Layout>
 )
 
